e4437b8a29dbe5d555d426d3ca85ae171d16bc90
Chris Teyerl <chris.teyerl@cosamey.com>
2025-08-21 19:34:34 +0200

    Clean up codebase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,62 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Number;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDefaults();
+        $this->configureModels();
+        $this->configureUrls();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureDefaults(): void
+    {
+        Number::useCurrency(config('app.currency'));
+        Number::useLocale(config('app.locale'));
+    }
+
+    private function configureModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+        Model::shouldBeStrict(app()->isLocal());
+
+        /** @var array<string, class-string<Model>> $morphMap */
+        $morphMap = collect(File::files(app_path('Models')))
+            ->mapWithKeys(function (\SplFileInfo $file): array {
+                $fileName = $file->getBasename('.php');
+                $namespace = 'App\\Models\\'.$fileName;
+
+                return is_subclass_of($namespace, Model::class)
+                    ? [str($fileName)->snake()->toString() => $namespace]
+                    : [];
+            })
+            ->toArray();
+
+        Relation::enforceMorphMap($morphMap);
+    }
+
+    private function configureUrls(): void
+    {
+        URL::forceHttps(app()->isProduction());
     }
 }
