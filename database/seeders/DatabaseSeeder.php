29010bab426e5b2cc3870bf24f30ba8e52b069c7
Chris Teyerl <chris.teyerl@cosamey.com>
2025-08-21 19:55:49 +0200

    Install Spatie Media Library v11

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,22 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->deleteMedia();
+
         $this->call([
             UserSeeder::class,
         ]);
+    }
+
+    private function deleteMedia(): void
+    {
+        File::deleteDirectory(storage_path('app/public/media'));
+        File::deleteDirectory(storage_path('app/private/media'));
     }
 }
