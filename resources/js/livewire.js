8b6ca4fdafe840f1b740f6f9fbe8ac653d376d0b
Chris Teyerl <chris.teyerl@gmail.com>
2026-06-18 17:19:41 +0200

    Install Alpine.js v3

--- resources/js/livewire.js
+++ resources/js/livewire.js
@@ -1,3 +1,12 @@
-import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import { Alpine, Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import collapse from '@alpinejs/collapse';
+import focus from '@alpinejs/focus';
+import mask from '@alpinejs/mask';
+import ui from '@alpinejs/ui';
+
+Alpine.plugin(collapse);
+Alpine.plugin(focus);
+Alpine.plugin(mask);
+Alpine.plugin(ui);
 
 Livewire.start();
